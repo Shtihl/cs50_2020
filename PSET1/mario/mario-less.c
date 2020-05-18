3a82cbe8b0a311b7e1a72e27ef626fa03f47fab5
Dmitriy Firsov <shtihlfd@yandex.ru>
2020-05-19 00:20:39 +0300

    first tasks add

---
+++ PSET1/mario/mario-less.c
@@ -0,0 +1,31 @@
+// Prints one pyramid of #
+// Based on World 1-1 in NES Super Mario
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+
+    // get user input until it will be between 1 and 8 inclusive
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // build pyramid
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = height - i; j > 0; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
